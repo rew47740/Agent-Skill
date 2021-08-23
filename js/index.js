a90f5d49a3dab7c554de0c4b469889fbdd9621fa
rew47740 <rew_love123@hotmail.com>
2021-08-23 12:21:41 +0700

    up

--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ fetch("json/data.json")
     var agentlevelcheck=0;
     var managercheck=0;
     var teamcheck=0;
+    var sortp=0;
     dataused=data.filter(function(obj) {return obj.TEAM=="Digital"}); 
     showdata();
     function filterAgentlevel (filter){
         dataused = data.filter(function(obj) {return obj.P_AGENT_LEVEL==filter});
+        selected.innerHTML = "Agent Level > "+filter+" ";
         // agentlevelcheck=1;
         // if (managercheck==1){
         //     dataused = dataused.filter(function(obj) {return obj.P_AGENT_LEVEL==filter});
@@ -21,6 +23,7 @@ fetch("json/data.json")
     }
     function filterManager (filter){
         dataused = data.filter(function(obj) {return obj.MANAGER_FULLNAME==filter}); 
+        selected.innerHTML = "Manager > "+filter;
         // managercheck=1;        
         // if (agentlevelcheck==1){
         //     dataused = dataused.filter(function(obj) {return obj.MANAGER_FULLNAME==filter});
@@ -34,6 +37,7 @@ fetch("json/data.json")
     }
     function filterTeam (filter){
         dataused = data.filter(function(obj) {return obj.TEAM==filter}); 
+        selected.innerHTML = "Team > "+filter;
         // teamcheck=1;   
         // if (agentlevelcheck==1){
         //     dataused = dataused.filter(function(obj) {return obj.TEAM==filter});
@@ -47,6 +51,7 @@ fetch("json/data.json")
     }
     function filterSkill (filter){
         dataused = data.filter(function(obj) {return obj.Main_Skill==filter}); 
+        selected.innerHTML = "Main Skill > "+filter;
     }
     function showdata(){ 
         show.innerHTML = " ";           
@@ -59,6 +64,37 @@ fetch("json/data.json")
                 show.innerHTML += '<div class="permanent" tabindex="0">'+"<span class = agentname>"+dataused[i].SALE_FULLNAME_ENG+"</span>"+'<div style="background: rgb(182, 0, 0);" class = status >&nbsp</div>'+"<br />"+"<span class = bold>Agent ID : </span>"+dataused[i].Agent_ID+"<br />"+"<span class = bold>Agent Level : </span>"+
                 dataused[i].P_AGENT_LEVEL+"<br />"+"<span class = bold>SUP. Fullname : </span>"+dataused[i].SUP_FULLNAME+"<br />"+"<span class = bold>Manager Fullname : </span>"+dataused[i].MANAGER_FULLNAME+"<br />"+"<span class = bold>Team : </span>"+dataused[i].TEAM+"<br />"+"<span class = bold>Status : </span>"+dataused[i].Status+"<br />"+'<div class="RowLv">'+'<div class = Column>'+"<span class = bold>Main Skill</span>"+"<br />"+dataused[i].Main_Skill+'</div>'+'<div class = Column>'+"<span class = bold>2nd Skill</span>"+"<br />"+dataused[i].two_Skill+'</div>'+'<div class = Column>'+"<span class = bold>3rd Skill</span>"+"<br />"+dataused[i].three_Skill+'</div>'+'<div class = Column>'+"<span class = bold>4th Skill</span>"+"<br />"+dataused[i].four_Skill+'</div>'+'</div>'+'<p class="seemore">▾</p>'+'<div class = RowLv>'+'<div class = Column>'+"<span class = bold>5th Skill</span>"+"<br />"+dataused[i].five_Skill+'</div>'+'<div class = Column>'+"<span class = bold>6th Skill</span>"+"<br />"+dataused[i].six_Skill+'</div>'+'<div class = Column>'+"<span class = bold>7th Skill</span>"+"<br />"+dataused[i].seven_Skill+'</div>'+'<div class = Column>'+"<span class = bold>8th Skill</span>"+"<br />"+dataused[i].eight_Skill+'</div>'+'</div>'+'<div class="Row">'+'<div class="Left">'+"<span class = bold>SUB XTVS : </span>"+dataused[i].SUB_XTVS+"<br />"+"<span class = bold>SUB XTOL : </span>"+dataused[i].SUB_XTOL+"<br />"+"<span class = bold>SUB XTMH : </span>"+dataused[i].SUB_XTMH+"<br />"+"<span class = bold>SUB UTVS : </span>"+dataused[i].SUB_UTVS+"<br />"+"<span class = bold>SUB UTOL : </span>"+dataused[i].SUB_UTOL+"<br />"+"<span class = bold>SUB UTMH : </span>"+dataused[i].SUB_UTMH+"<br />"+"<span class = bold>TOTAL SUB X : </span>"+dataused[i].TOTAL_SUB_X+"<br />"+"<span class = bold>TOTAL SUB U : </span>"+dataused[i].TOTAL_SUB_U+"<br />"+"<span class = bold>TOTAL SUB X+U : </span>"+dataused[i].TOTAL_SUB_X_U+"<br />"+'</div>'+'<div class="Right">'+"<span class = bold>Revenue XTVS : </span>"+dataused[i].Revenue_XTVS+"<br />"+"<span class = bold>Revenue XTOL : </span>"+dataused[i].Revenue_XTOL+"<br />"+"<span class = bold>Revenue XTMH : </span>"+dataused[i].Revenue_XTMH+"<br />"+"<span class = bold>Revenue UTVS : </span>"+dataused[i].Revenue_UTVS+"<br />"+"<span class = bold>Revenue UTOL : </span>"+dataused[i].Revenue_UTOL+"<br />"+"<span class = bold>Revenue UTMH : </span>"+dataused[i].Revenue_UTMH+"<br />"+"<span class = bold>TOTAL Revenue X : </span>"+dataused[i].TOTAL_Revenue_X+"<br />"+"<span class = bold>TOTAL Revenue U : </span>"+dataused[i].TOTAL_Revenue_U+"<br />"+"<span class = bold>TOTAL Revenue X+U : </span>"+dataused[i].TOTAL_Revenue_X_U+"<br />"+'</div>'+'</div>'+'</div>'; 
             }
+        }
+    }
+    function sortrev(prop) { 
+        console.log("kuy")
+        if(sortp==0){
+        dataused = dataused.sort((a, b) => {
+            let retval = 0;
+            if (a.TOTAL_Revenue_X_U < b.TOTAL_Revenue_X_U)
+              retval = -1;
+            if (a.TOTAL_Revenue_X_U > b.TOTAL_Revenue_X_U)
+              retval = 1;
+            if (retval === 0)
+              retval = a.dataused > b.dataused ? -1 : 1;
+            return retval;
+          });
+          sortp=1;
+          btnsort.innerHTML = "Sort by Revenue ⇂";
+        }
+        else if(sortp==1){
+        dataused = dataused.sort((a, b) => {
+            let retval = 0;
+            if (a.TOTAL_Revenue_X_U > b.TOTAL_Revenue_X_U)
+              retval = -1;
+            if (a.TOTAL_Revenue_X_U < b.TOTAL_Revenue_X_U)
+              retval = 1;
+            if (retval === 0)
+              retval = a.dataused < b.dataused ? -1 : 1;
+            return retval;
+          });
+          sortp=0;
+          btnsort.innerHTML = "Sort by Revenue ↾";
         }
     }
     document.getElementById('btncontract').onclick = function(){
@@ -156,5 +192,9 @@ fetch("json/data.json")
     document.getElementById('btnutvs').onclick = function(){
         filterSkill('Up TVS');
         showdata();
-    }    
+    }   
+    document.getElementById('btnsort').onclick = function(){
+        sortrev();
+        showdata();
+    }     
 })
